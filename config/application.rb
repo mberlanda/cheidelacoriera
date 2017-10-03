7fbeece1b23dea5a56925ce7d0f5284e06648f05
Mauro Berlanda <mauro.berlanda@mecglobal.com>
2017-10-03 10:29:23 +0200

    Added a few locales

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,9 @@ module Cheidelacoriera
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.available_locales = %i[en it]
+    # config.i18n.default_locale = :it
   end
 end
