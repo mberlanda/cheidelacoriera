d6621b8a33a7861e707c529201a42e12b1c59294
Mauro Berlanda <mauro.berlanda@mecglobal.com>
2017-10-03 12:49:27 +0200

    Added Internalization support for multilanguage app

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,6 @@ module Cheidelacoriera
 
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.available_locales = %i[en it]
-    # config.i18n.default_locale = :it
+    config.i18n.default_locale = :it
   end
 end
