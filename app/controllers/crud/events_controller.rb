a820b4852819ffe0435bb9ddcba8eec2fa65bc8d
Mauro Berlanda <mauro.berlanda@mecglobal.com>
2017-10-10 10:41:31 +0200

    Added venue to permitted attrs in Events CRUD controller

--- app/controllers/crud/events_controller.rb
+++ app/controllers/crud/events_controller.rb
@@ -3,6 +3,7 @@
 class Crud::EventsController < CrudController
   before_action :authenticate_user!
   self.permitted_attrs = %i[date time season score notes home_team_id
-                            away_team_id competition_id]
-  self.search_columns = %i[date season home_team_id away_team_id competition_id]
+                            away_team_id competition_id venue]
+  self.search_columns = %i[date season home_team_id away_team_id competition_id
+                           venue]
 end
