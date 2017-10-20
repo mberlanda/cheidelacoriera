45d6926c62d7a07ba8416c6689e097f88f3ae6ef
Mauro Berlanda <mauro.berlanda@mecglobal.com>
2017-10-20 17:26:11 +0200

    Added some call to action in the homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,10 @@
 
 class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
-  def index; end
+  def index
+    @actions = {
+      fans: fans_path,
+      upcoming_events: upcoming_events_path
+    }
+  end
 end
