9af0b0c6f6eff8ede32378e6c502ec968f5a701e
Mauro Berlanda <mauro.berlanda@gmail.com>
2017-11-06 08:16:29 +0100

    Used a scrubber to prevent Cross Scripting

---
+++ app/scrubbers/event_scrubber.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class EventScrubber < Rails::Html::PermitScrubber
+  def initialize
+    super
+    self.tags = %w[a blockquote b br div em font i li ol p pre u ul]
+    self.attributes = %w[align color face href size style target title]
+  end
+
+  def skip_node?(node)
+    node.text?
+  end
+end
