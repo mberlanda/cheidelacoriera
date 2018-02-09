496cee33c8112752e86b237209d006326c4a7fa3
Mauro Berlanda <mauro.berlanda@gmail.com>
2018-02-09 20:06:44 +0100

    feat(webpacker): added vendor bundle for js node_modules

---
+++ app/javascript/packs/vendor.js
@@ -0,0 +1,12 @@
+import 'jquery/dist/jquery.js';
+import 'jquery-ujs/src/rails.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'jquery-backstretch/jquery.backstretch.min.js';
+import 'waypoints/lib/jquery.waypoints.js';
+import 'jquery-timepicker/jquery.timepicker.js';
+import 'bootstrap-datepicker/js/bootstrap-datepicker.min.js';
+import 'bootstrap-datepicker/dist/locales/bootstrap-datepicker.it.min.js';
+import 'multiselect/js/jquery.multi-select.js';
+import 'wowjs/dist/wow.min.js';
+import 'retinajs/dist/retina.min.js';
+import 'intro.js/intro.js';
