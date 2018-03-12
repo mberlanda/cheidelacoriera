f1d0c89bae897156af30356984d411dd850f2f46
Mauro Berlanda <mauro.berlanda@gmail.com>
2018-03-12 21:23:28 +0100

    fix(ie11): remove ES6 arrow functions

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,5 +2,6 @@ const { environment } = require('@rails/webpacker')
 const customConfig = require('./custom')
 
 environment.config.merge(customConfig)
+environment.plugins.get("UglifyJs").options.uglifyOptions.ecma = 5
 
 module.exports = environment
