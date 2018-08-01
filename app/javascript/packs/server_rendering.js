b46adb700a17cf23bd7632604c2d70908da70363
Mauro Berlanda <mauro.berlanda@gmail.com>
2018-08-01 17:12:05 +0200

    added babel-polyfill for server-side rendering

--- app/javascript/packs/server_rendering.js
+++ app/javascript/packs/server_rendering.js
@@ -3,3 +3,4 @@
 var componentRequireContext = require.context("components", true)
 var ReactRailsUJS = require("react_ujs")
 ReactRailsUJS.useContext(componentRequireContext)
+import 'babel-polyfill';
