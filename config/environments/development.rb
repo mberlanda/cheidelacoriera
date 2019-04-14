0a2105f4bf94eafc33ed94cca572c49bc88e2a78
Mauro Berlanda <mauroberlanda@gmail.com>
2019-04-14 23:16:19 +0200

    configure 5.1 framework defaults (#47)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.configure do
-  config.webpacker.check_yarn_integrity = true # Settings specified here will take precedence over those in config/application.rb.
+  # Settings specified here will take precedence over those in config/application.rb.
+  config.webpacker.check_yarn_integrity = true
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
@@ -20,7 +21,7 @@ Rails.application.configure do
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      'Cache-Control' => 'public, max-age=172800'
+      'Cache-Control' => "public, max-age=#{2.days.seconds.to_i}"
     }
   else
     config.action_controller.perform_caching = false
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.configure do
-  config.webpacker.check_yarn_integrity = false # Settings specified here will take precedence over those in config/application.rb.
+  # Settings specified here will take precedence over those in config/application.rb.
+  config.webpacker.check_yarn_integrity = false
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -16,12 +17,17 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
+  # Requires an encryption key in `ENV["RAILS_MASTER_KEY"]` or
+  # `config/secrets.yml.key`.
+  config.read_encrypted_secrets = false
+
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = Uglifier.new(harmony: true)
+  config.assets.js_compressor = Uglifier.new(harmony: true) # :uglifier
   config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
@@ -80,7 +86,7 @@ Rails.application.configure do
   if ENV['RAILS_LOG_TO_STDOUT'].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
-    config.logger = ActiveSupport::TaggedLogging.new(logger)
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
   # Do not dump schema after migrations.
@@ -110,6 +116,5 @@ Rails.application.configure do
   config.action_mailer.asset_host = 'http://www.cheidelacoriera.com'
 
   config.action_mailer.default_url_options = { host: 'www.cheidelacoriera.com' }
-
   config.force_ssl = true if ENV['RAILS_FORCE_SSL'].present?
 end
