d43875292d59a3d6723de28bdc8bf3d656be7aca
Mauro Berlanda <mauro.berlanda@gmail.com>
2019-05-08 16:46:28 +0200

    use chunks

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,4 +3,8 @@ const customConfig = require('./custom')
 
 environment.config.merge(customConfig);
 
+// https://github.com/rails/webpacker/blob/master/docs/v4-upgrade.md#splitchunks-configuration
+// Enable the default config
+environment.splitChunks()
+
 module.exports = environment
