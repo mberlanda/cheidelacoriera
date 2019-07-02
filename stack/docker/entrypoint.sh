de39c13900723eac9c2132f08a337d00c699b8b7
Mauro Berlanda <mauro.berlanda@gmail.com>
2019-07-02 14:39:35 +0200

    refine image

--- stack/docker/entrypoint.sh
+++ stack/docker/entrypoint.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 IFS=$'\n\t'
 
 function main {
-  if [[ "${RAILS_DB_MIGRATE}" == "true" ]];
+  if [[ "${RAILS_DB_MIGRATE}" == "true" ]]; then
     bundle exec rake db:migrate
   fi
 }
