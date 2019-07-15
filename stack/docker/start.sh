4357971ff330e8be6ca163d4169fd18c6cc8a142
Mauro Berlanda <mauro.berlanda@gmail.com>
2019-07-15 17:17:14 +0200

    chore: added basic nginx config
    https://blog.codeship.com/how-to-deploy-nginx-on-heroku/

--- stack/docker/start.sh
+++ stack/docker/start.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 IFS=$'\n\t'
 
 function main {
+  sudo service nginx restart
   nohup bundle exec sidekiq -e production -C config/sidekiq.yml </dev/null >/dev/null 2>&1 & \
   bundle exec puma -C config/puma.rb
 }
