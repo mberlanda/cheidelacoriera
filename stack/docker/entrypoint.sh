b99c66434931f6ce658ee111e280b1b6f079d36d
Mauro Berlanda <mauro.berlanda@gmail.com>
2019-07-28 18:09:25 +0200

    chore: nginx config

--- stack/docker/entrypoint.sh
+++ stack/docker/entrypoint.sh
@@ -2,11 +2,18 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+PORT=${PORT:-3001}
+
 if [[ "${RAILS_DB_MIGRATE}" == "true" ]]; then
   bundle exec rake db:migrate
 fi
 
-# Path to Puma PID and SOCK file
+# Path to PIDS and SOCK file
 mkdir -p shared/pids shared/sockets
+
+# Substitute $PORT in nginx config
+sed -i -e 's/$PORT/'"$PORT"'/g' /etc/nginx/conf.d/default.conf
+# nginx -s reload
+/etc/init.d/nginx restart
 
 exec "$@"
--- stack/docker/start.sh
+++ stack/docker/start.sh
@@ -3,7 +3,6 @@ set -euo pipefail
 IFS=$'\n\t'
 
 function main {
-  sudo service nginx restart
   nohup bundle exec sidekiq -e production -C config/sidekiq.yml </dev/null >/dev/null 2>&1 & \
   bundle exec puma -C config/puma.rb
 }
