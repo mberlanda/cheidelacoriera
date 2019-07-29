8ae3827452d998bc3d480a01379fcb3d3bcd9d7c
Mauro Berlanda <mauro.berlanda@gmail.com>
2019-07-29 23:48:00 +0200

    Add Nginx (#72)
    * chore: prepare puma config for nginx
    
    https://www.digitalocean.com/community/tutorials/how-to-deploy-a-rails-app-with-puma-and-nginx-on-ubuntu-14-04
    
    * chore: added basic nginx config
    
    https://blog.codeship.com/how-to-deploy-nginx-on-heroku/
    
    * chore: nginx config
    
    * fix puma and nginx config
    
    * config tuning
    
    * cleanup

--- stack/docker/entrypoint.sh
+++ stack/docker/entrypoint.sh
@@ -2,8 +2,16 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+PORT=${PORT:-3001}
+
 if [[ "${RAILS_DB_MIGRATE}" == "true" ]]; then
   bundle exec rake db:migrate
 fi
+
+# Path to PIDS and SOCK file
+mkdir -p shared/pids shared/sockets
+
+# Substitute $PORT in nginx config
+sed -i -e 's/$PORT/'"$PORT"'/g' /etc/nginx/sites-available/default
 
 exec "$@"
