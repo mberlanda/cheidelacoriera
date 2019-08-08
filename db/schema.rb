2e1d21d43fcde441edc43fa8d775be1b4d6b0b20
Mauro Berlanda <mauro.berlanda@gmail.com>
2019-08-08 16:52:28 +0200

    db: added stop to reservations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_07_201231) do
+ActiveRecord::Schema.define(version: 2019_08_08_145009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,6 +95,7 @@ ActiveRecord::Schema.define(version: 2019_08_07_201231) do
     t.integer "user_id"
     t.integer "event_id"
     t.date "mail_sent"
+    t.string "stop"
     t.index ["event_id"], name: "index_reservations_on_event_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
