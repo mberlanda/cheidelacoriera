db6ef1301f7141f9f87d2c02bcebb3589a5c43a8
Mauro Berlanda <mauro.berlanda@gmail.com>
2019-08-14 13:45:25 +0200

    fix woker configuration

--- bin/release-worker.sh
+++ bin/release-worker.sh
@@ -4,7 +4,7 @@ IFS=$'\n\t'
 
 tag="$1"
 
-docker build -t "cheidelacoriera:${tag}" -f Dockerfile.web .
+docker build -t "cheidelacoriera:${tag}" -f Dockerfile.worker .
 docker tag "cheidelacoriera:${tag}" registry.heroku.com/cheidelacoriera/worker
 docker push registry.heroku.com/cheidelacoriera/worker
 heroku container:release worker -a cheidelacoriera
