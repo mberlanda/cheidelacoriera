67a32fa87dcd44a9a90b815f86a46e106e93f583
Mauro Berlanda <mauro.berlanda@gmail.com>
2019-09-21 15:16:08 +0200

    test: reproduce n+1 on ReservationsController#approve_all

--- spec/requests/reservations_approve_all_spec.rb
+++ spec/requests/reservations_approve_all_spec.rb
@@ -2,5 +2,27 @@
 
 require 'rails_helper'
 
-RSpec.describe 'ReservationsController#approve_all' do
+RSpec.describe 'ReservationsController#approve_all', type: :request do
+  let(:admin) { FactoryGirl.create(:user, role: :admin, status: :active) }
+  let(:event) { FactoryGirl.create(:event) }
+  let!(:reservation_1) do
+    FactoryGirl.create(
+      :reservation,
+      event: event,
+      fan_names: %w[Foo|Bar]
+    )
+  end
+  let!(:reservation_2) do
+    FactoryGirl.create(
+      :reservation,
+      event: event,
+      fan_names: %w[Bar|Baz]
+    )
+  end
+
+  it 'prevents n+1 queries' do
+    sign_in admin
+
+    get '/reservations/approve_all', params: { event_id: event.id, slug: event.slug }
+  end
 end
