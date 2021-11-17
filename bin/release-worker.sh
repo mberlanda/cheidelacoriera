4974ec5e0440ecf82fa6b789ed04e96fd53d1065
Mauro Berlanda <mauro.berlanda@gmail.com>
2021-11-17 22:26:29 +0100

    chore: revisit release script to share image layers between staging and prod

--- bin/release-worker.sh
+++ bin/release-worker.sh
@@ -3,8 +3,11 @@ set -euo pipefail
 IFS=$'\n\t'
 
 tag="$1"
+# This env var allows to re-use the same docker cached
+# layers when building images for different environments
+app="${TARGET_APP:-cheidelcoriera}"
 
-docker build -t "cheidelacoriera:${tag}" -f Dockerfile.worker .
-docker tag "cheidelacoriera:${tag}" registry.heroku.com/cheidelacoriera/worker
-docker push registry.heroku.com/cheidelacoriera/worker
-heroku container:release worker -a cheidelacoriera
+docker build -t "${app}:${tag}" -f Dockerfile.worker .
+docker tag "${app}:${tag}" "registry.heroku.com/${app}/worker"
+docker push "registry.heroku.com/${app}/worker"
+heroku container:release worker -a "${app}"
--- bin/release.sh
+++ bin/release.sh
@@ -3,8 +3,11 @@ set -euo pipefail
 IFS=$'\n\t'
 
 tag="$1"
+# This env var allows to re-use the same docker cached
+# layers when building images for different environments
+app="${TARGET_APP:-cheidelcoriera}"
 
-docker build -t "cheidelacoriera:${tag}" -f Dockerfile.web .
-docker tag "cheidelacoriera:${tag}" registry.heroku.com/cheidelacoriera/web
-docker push registry.heroku.com/cheidelacoriera/web
-heroku container:release web -a cheidelacoriera
+docker build -t "${app}:${tag}" -f Dockerfile.web .
+docker tag "${app}:${tag}" "registry.heroku.com/${app}/web"
+docker push "registry.heroku.com/${app}/web"
+heroku container:release web -a "${app}"
