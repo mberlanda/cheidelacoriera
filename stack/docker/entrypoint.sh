4281bed5536f1297de6b67828287479892a4cd00
Mauro Berlanda <mauro.berlanda@gmail.com>
2022-04-10 13:29:44 +0200

    docker compose setup for local testing

--- stack/docker/entrypoint.sh
+++ stack/docker/entrypoint.sh
@@ -3,10 +3,14 @@ set -euo pipefail
 IFS=$'\n\t'
 
 PORT=${PORT:-3001}
+RAILS_DB_SEED=${RAILS_DB_SEED:-false}
 export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libjemalloc.so
 
 if [[ "${RAILS_DB_MIGRATE}" == "true" ]]; then
   bundle exec rake db:migrate
+fi
+if [[ "${RAILS_DB_SEED}" == "true" ]]; then
+  bundle exec rails db:seed
 fi
 
 # Path to PIDS and SOCK file
