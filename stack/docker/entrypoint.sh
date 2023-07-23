e2f8b300f9c2226eefc3c996c1c5135ffeb3b152
Mauro Berlanda <mauro.berlanda@gmail.com>
2023-07-23 11:34:05 +0200

    chore: upgrade to bullseye and ruby 2.7.8

--- stack/docker/entrypoint.sh
+++ stack/docker/entrypoint.sh
@@ -4,7 +4,7 @@ IFS=$'\n\t'
 
 PORT=${PORT:-3001}
 RAILS_DB_SEED=${RAILS_DB_SEED:-false}
-export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libjemalloc.so
+export LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libjemalloc.so
 
 if [[ "${RAILS_DB_MIGRATE}" == "true" ]]; then
   bundle exec rake db:migrate
