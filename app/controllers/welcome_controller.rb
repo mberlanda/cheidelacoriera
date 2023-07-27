cafdd5a1c101d49bb50f75b1578da19cf384e5ab
Mauro Berlanda <mauro.berlanda@gmail.com>
2023-07-27 23:45:02 +0200

    Revert "Comment out unused pages entrypoints"
    This reverts commit d1eb29f5d9d9c55aac438fbd41a1f370bb5b55b5.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!
 
@@ -13,10 +15,10 @@ class WelcomeController < ApplicationController
     default = current_user ? {} : { login: new_user_session_path }
     @actions = default.merge(
       upcoming_events: upcoming_events_path,
-      # news: news_index_path,
-      # posts: all_posts_path,
-      # photogallery: all_albums_path,
       facebook: FACEBOOK_GROUP_URL,
+      news: news_index_path,
+      posts: all_posts_path,
+      photogallery: all_albums_path,
       regolamento: regolamento_path
     ).map(&build_action)
   end
